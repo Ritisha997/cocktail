59aa22555cd3a42b616816d64d2be30dc7ca68df
Yash Dnyaneshwar Shelke <yashshelke885@gmail.com>
2023-03-02 21:11:01 +0530

    packge

--- Desktop/All files/my-design/src/components/SingleCocktail.js
+++ Desktop/All files/my-design/src/components/SingleCocktail.js
@@ -2,19 +2,20 @@ import React from 'react'
 import { useParams } from 'react-router-dom';
 import { setTheCocktail } from '../reducer';
 import { reducer } from '../reducer';
-
+import useDispatch from 'react-redux'
 
 const SingleCocktail = () => {
   // const fills = cocktail.filter(item =>)
   const url = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=";
   const {id} = useParams();
   console.log(useParams())
-  
+  const dispatch = useDispatch()
   const FetchSingle = async () =>{
     const response = await fetch(url);
     const data = await response.json();
     console.log(data)
   }
+  dispatch(setTheCocktail(data))
   React.useEffect(()=>{
     FetchSingle();
   },[])
